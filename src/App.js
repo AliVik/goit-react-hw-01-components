082440df271cacc48e117845a46c2496deab1ad6
AliVik <alvikul1994@gmail.com>
2021-12-25 18:10:29 +0200

    подключает компонент Статистика к главной функции Арр

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Profile from "./components/Profile/Profile";
+import Statistics from "./components/Statistics/Statistics";
 import user from "./user.json";
+import data from "./data.json";
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
         location={user.location}
         stats={user.stats}
       />
+
+      <Statistics key={data.id} title={data.title} stats={data} />
     </>
   );
 }
