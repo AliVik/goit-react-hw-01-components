423d4e8919c3b43a9e5d7d717d2f997c70849ae1
AliVik <alvikul1994@gmail.com>
2021-12-26 18:24:46 +0200

    добавляет компонент Транзакции и стили для него

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Profile from "./components/Profile/Profile";
 import Statistics from "./components/Statistics/Statistics";
 import FriendList from "./components/Friends/FriendList";
+import TransactionHistory from "./components/Transaction/TransactionHistory";
 import user from "./user.json";
 import data from "./data.json";
 import friends from "./friends.json";
+import transactions from "./transactions.json";
 
 export default function App() {
   return (
@@ -18,6 +20,7 @@ export default function App() {
 
       <Statistics key={data.id} title={data.title} stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 }
---
+++ src/components/Transaction/TransactionHistory.js
@@ -0,0 +1,36 @@
+import css from "./TransactionHistory.module.css";
+import PropTypes from "prop-types";
+
+export default function TransactionHistory({ items }) {
+  return (
+    <table className={css.transactionHistory}>
+      <thead className={css.transactionHistoryHead}>
+        <tr>
+          <th className={css.transactionHistoryHeadItem}>Type</th>
+          <th className={css.transactionHistoryHeadItem}>Amount</th>
+          <th className={css.transactionHistoryHeadItem}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map((item) => (
+          <tr key={item.id} className={css.transactionHistoryBlock}>
+            <td className={css.transactionHistoryItem}>{item.type}</td>
+            <td className={css.transactionHistoryItem}>{item.amount}</td>
+            <td className={css.transactionHistoryItem}>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      type: PropTypes.string,
+      amount: PropTypes.string,
+      currency: PropTypes.string,
+    })
+  ),
+};
